740ca297f264cb4f6d5e9106ae23546be971fc97
Arvin <arvincris@gmail.com>
2024-11-14 10:14:39 +0800

    Hero Section: Implementing Sparkles

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,6 +4,7 @@ import ArrowDown from '@/assets/icons/arrow-down.svg';
 import grainImage from '@/assets/images/grain.jpg'
 import StarIcon from '@/assets/icons/star.svg';
 import { HeroOrbit } from '@/components/HeroOrbit';
+import SparkleIcon from '@/assets/icons/sparkle.svg';
 
 export const HeroSection = () => {
   return (
@@ -24,12 +25,24 @@ export const HeroSection = () => {
       <HeroOrbit size={590} rotation={98}>
         <StarIcon className="size-8 text-emerald-300"/>
       </HeroOrbit>
+      <HeroOrbit size={430} rotation={-14}>
+        <SparkleIcon className="size-8 text-emerald-300/20"/>
+      </HeroOrbit>
+      <HeroOrbit size={440} rotation={79}>
+        <SparkleIcon className="size-5 text-emerald-300/20"/>
+      </HeroOrbit>
+      <HeroOrbit size={530} rotation={178}>
+        <SparkleIcon className="size-10 text-emerald-300/20"/>
+      </HeroOrbit>
+      <HeroOrbit size={710} rotation={144}>
+        <SparkleIcon className="size-14 text-emerald-300/20"/>
+      </HeroOrbit>
       <div className="container">
         <div className="flex flex-col items-center">
           <Image src={memojiImage} className="size-[100px]" alt="Person Peeking from behind"/>
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
             <div className="bg-green-500 size-2.5 rounded-full"></div>
-            <div className="text-sm font-medium">Available for new projects</div>
+            <div className="text-sm font-medium">PRG-Arvin - Available for new projects</div>
           </div>
         </div>
         <div className='max-w-lg mx-auto'>

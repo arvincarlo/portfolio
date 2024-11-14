066215312759648757f03b0f9ffdba048b009c81
Arvin <arvincris@gmail.com>
2024-11-14 10:50:27 +0800

    Hero Section: masking gradient & Circles

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -8,12 +8,12 @@ export const HeroOrbit = ({
     return (
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
             {/* Stars animation */}
-            <div style={{
+            <div className="flex items-start justify-start" style={{
                 height: `${size}px`,
                 width: `${size}px`,
                 transform: `rotate(${rotation}deg)`
             }}>
-                <div className=" inline-flex" style={{
+                <div className="inline-flex" style={{
                     transform: `rotate(${rotation * -1}deg)`
                 }}>
                     {children}

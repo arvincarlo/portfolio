51028bfe7af007a0ef1919b0406ca132328393c7
Arvin <arvincris@gmail.com>
2024-11-15 12:48:40 +0800

    About Section: Bento Box for tablet (books and toolbox)

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -93,17 +93,21 @@ export const AboutSection = () => {
       <div className="container">
         <SectionHeader eyebrow="About Me" title="A Glimpse Into My World" description="Learn more about who I am, what I do, and what inspires me."/>
         <div className="mt-20 flex flex-col gap-8">
-          <Card className="h-[320px]">
-            <CardHeader title="My Reads" description="Explore the books shaping my perspectives."/>
-            <div className="w-40 mx-auto mt-8">
-              <Image src={bookImage} alt="book cover"/>
-            </div>
-          </Card>
-          <Card className="h-[320px] p-0">
-            <CardHeader title="My Toolbox" description="Explore the technologies and tools I use to craft exceptional digital experiences." className="px-6 pt-6"/>
-            <ToolboxItems items={toolboxItems} className="mt-6"/>
-            <ToolboxItems items={toolboxItems} className="mt-6" itemsWrapperClassName="-translate-x-1/2"/>
-          </Card>
+          <div className="grid sm:grid-cols-1 gap-8 md:grid-cols-5">
+            <Card className="h-[320px] md:col-span-2">
+              <CardHeader title="My Reads" description="Explore the books shaping my perspectives."/>
+              <div className="w-40 mx-auto mt-2 md:mt-0">
+                <Image src={bookImage} alt="book cover"/>
+              </div>
+            </Card>
+
+            <Card className="h-[320px] md:col-span-3">
+              <CardHeader title="My Toolbox" description="Explore the technologies and tools I use to craft exceptional digital experiences." className=""/>
+              <ToolboxItems items={toolboxItems} className=""/>
+              <ToolboxItems items={toolboxItems} className="mt-6" itemsWrapperClassName="-translate-x-1/2"/>
+            </Card>
+          </div>
+
           <Card className="h-[320px] p-0 flex flex-col">
             <CardHeader title="Beyond the Code" description="Explore my interests and hobbies beyond the digital realm." className="px-6 py-6"/>
             <div className="relative flex-1">
@@ -118,12 +122,14 @@ export const AboutSection = () => {
               ))}
             </div>
           </Card>
+
           <Card className="h-[320px] p-0 relative">
             <Image src={mapImage} alt="map" className="h-full w-full object-cover object-left-top"/>
             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
               <Image src={smileMemoji} alt="smile emoji" className="size-20"/>
             </div>
           </Card>
+
         </div>
       </div>
     </div>

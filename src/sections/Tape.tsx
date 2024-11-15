6e671d606436068fb33c2ccc9072a83a0dab3255
Arvin <arvincris@gmail.com>
2024-11-15 16:42:00 +0800

    Tape animation

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,4 +1,5 @@
 import StarIcon from "@/assets/icons/star.svg";
+import { Fragment } from "react";
 
 const words = [
   "Performant",
@@ -20,15 +21,21 @@ export const TapeSection = () => {
     <div className="py-16 lg:py-24 overflow-x-clip">
       <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1">
         <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex flex-none gap-4 py-3">
-            {
-              words.map(word => (
-                <div className="inline-flex gap-4 items-center" key={word}>
-                  <span className="text-gray-900 uppercase font-extrabold text-sm">{word}</span>
-                  <StarIcon className="size-6 text-gray-900 -rotate-12"/>
-                </div>
-              ))
-            }
+          <div className="flex flex-none gap-4 py-3 pr-4 animate animate-move-left [animation-duration:30s]" style={{
+            transform: 'translateX(-750px)'
+          }}>
+            {[...new Array(2)].fill(0).map((_, index) => (
+              <Fragment key={index}>
+                {
+                  words.map(word => (
+                    <div className="inline-flex gap-4 items-center" key={word}>
+                      <span className="text-gray-900 uppercase font-extrabold text-sm">{word}</span>
+                      <StarIcon className="size-6 text-gray-900 -rotate-12"/>
+                    </div>
+                  ))
+                }
+              </Fragment>
+            ))}
           </div>
         </div>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,7 @@ const config: Config = {
       },
       animation: {
         'ping-large': "ping-large 1s ease-in-out infinite",
+        'move-left': 'move-left 1s linear infinite'
       },
       keyframes: {
         'ping-large': {
@@ -34,8 +35,16 @@ const config: Config = {
             transform: 'scale(3)',
             opacity: '0',
           }
+        },
+        'move-left': {
+          '0%': {
+            transform: 'translateX(0%)'
+          },
+          '100%': {
+            transform: 'translateX(-50%)'
+          },
         }
-      }
+      },
     },
   },
   plugins: [],

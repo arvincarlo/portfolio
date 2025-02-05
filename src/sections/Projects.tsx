6d6ab022c2829ed39bb941015c30e6039e8e09c1
Arvin Cris <arvincris97@gmail.com>
2025-02-05 14:01:55 +0800

    updated description

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -25,7 +25,7 @@ const portfolioProjects = [
       results: [
         { title: "Data Fetching and Server Actions" },
         { title: "Client and Server Components" },
-        { title: "Auth.js for authentication" },
+        { title: "Auth.js for Google authentication" },
       ],
       link: "https://sunny-wild-oasis.netlify.app/",
       image: sunny,
